68c341a1c3ad8efa0980e92388460c55ba1c4982
Tupukda <teemu.s.harma@gmail.com>
2025-04-10 12:28:49 +0300

    5

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -79,8 +79,58 @@ describe('Blog App', () => {
             await page.locator('button[type="submit"]').click()
 
             await page.getByRole('button', { name: 'view' }).click()
-            await page.getByRole('button', { name: 'remove' }).click()
             
+            page.on('dialog', async dialog => {
+              await dialog.accept()
+            })
+            
+            await page.getByRole('button', { name: 'remove' }).click()
+
+            await expect(page.locator('div[name="Title"]').filter({ hasText: 'Test Blog' })).not.toBeVisible()
+            
+          })
+
+          test('only creator can delete a blog', async ({ page, request }) => {
+            await page.locator('button').filter({ hasText: 'Create new blog' }).click()
+            await page.locator('input[name="Title"]').fill('Test Blog')
+            await page.locator('input[name="Author"]').fill('Test Author')
+            await page.locator('input[name="Url"]').fill('http://testblog.com')
+            await page.locator('button[type="submit"]').click()
+            await page.locator('button').filter({ hasText: 'logout' }).click()
+            await request.post('http://localhost:3003/api/users', {
+              data: {
+                name: 'Test User',
+                username: 'testuser',
+                password: 'password'
+              }
+            })
+            await page.locator('input[name="Username"]').fill('testuser')
+            await page.locator('input[name="Password"]').fill('password')
+            await page.locator('button[type="submit"]').click()
+
+            await page.getByRole('button', { name: 'view' }).click()
+
+            await expect(page.locator('button').filter({ hasText: 'remove' })).not.toBeVisible()
+          })
+
+          test('blogs are ordered by likes', async ({ page }) => {
+            await page.locator('button').filter({ hasText: 'Create new blog' }).click();
+            await page.locator('input[name="Title"]').fill('Blog 1');
+            await page.locator('input[name="Author"]').fill('Author 1');
+            await page.locator('input[name="Url"]').fill('http://blog1.com');
+            await page.locator('button[type="submit"]').click();
+
+            await page.locator('button').filter({ hasText: 'Create new blog' }).click();
+            await page.locator('input[name="Title"]').fill('Blog 2');
+            await page.locator('input[name="Author"]').fill('Author 2');
+            await page.locator('input[name="Url"]').fill('http://blog2.com');
+            await page.locator('button[type="submit"]').click();
+
+            await page.pause()
+            await page.getByText('view').nth(1).click()
+            await page.locator('button[name="Like"]').nth(0).click()
+
+            await expect(page.locator('div[name="Likes"]').nth(0).filter({ hasText: '1' })).toBeVisible()
           })
         })
       })
